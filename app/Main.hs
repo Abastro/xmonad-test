4cb3cbeebe0b40cff9fae6b55d6f3e315f7a877c
Abastro <abab9579@gmail.com>
2022-04-05 19:06:47 +0900

    Established xmonad-test

---
+++ app/Main.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import XMonad
+import XMonad.Config.Desktop
+import XMonad.Config.Gnome
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.SetWMName
+import XMonad.Util.EZConfig
+import XMonad.Util.Run
+
+main :: IO ()
+main = do
+  --dirs <- getDirectories
+  --let xmDir = cfgDir dirs
+  spawn "feh --bg-scale $HOME/Pictures/Universe/Andromeda_Galaxy.jpg"
+
+  xmonad . ewmhFullscreen $
+    desktopConfig
+      { modMask = mod4Mask,
+        startupHook = startupHook desktopConfig <> do
+          setWMName "LG3D"
+          gnomeRegister, -- Registers xmonad with gnome
+        terminal = "gnome-terminal"
+        --manageHook =  isDialog --> doFloat
+      }
+      `additionalKeysP` [("M-d", safeSpawnProg "nautilus")]
