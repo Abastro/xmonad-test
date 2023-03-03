03a49da4e2610e5e7ae7b37a0325f39496a74a45
Abastro <abab9579@gmail.com>
2023-03-03 22:25:13 +0900

    Add basic functionalities back

--- app/Main.hs
+++ app/Main.hs
@@ -4,25 +4,19 @@ import XMonad
 import XMonad.Config.Desktop
 import XMonad.Config.Gnome
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.SetWMName
-import XMonad.Util.EZConfig
-import XMonad.Util.Run
 
 main :: IO ()
 main = do
-  --dirs <- getDirectories
-  --let xmDir = cfgDir dirs
   spawn "feh --bg-scale /home/abastro/Pictures/Universe/Andromeda_Galaxy.jpg"
-  xmonad desktopConfig
-  {-
   xmonad . ewmhFullscreen $
     desktopConfig
-      { --modMask = mod4Mask,
-        --startupHook = startupHook desktopConfig <> do
-        --  setWMName "LG3D"
-        --  gnomeRegister, -- Registers xmonad with gnome
-        terminal = "gnome-terminal"
-        --manageHook =  isDialog --> doFloat
+      { modMask = mod4Mask
+      , startupHook =
+          startupHook desktopConfig <> do
+            setWMName "LG3D"
+            gnomeRegister -- Registers xmonad with gnome
+      , terminal = "gnome-terminal"
+      , manageHook = isDialog --> doFloat
       }
-      `additionalKeysP` [("M-d", safeSpawnProg "nautilus")]
-      -}
