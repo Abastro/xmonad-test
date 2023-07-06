3c38142c7fd1c7a115732b9c52fe61b57582e4ec
Abastro <abab9579@gmail.com>
2023-07-06 23:00:43 +0900

    Change env usage

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PROF_ID="xmonad-test"
-XMONAD_EXE_PATH="$XMONAD_CACHE_DIR/xmonad-$ENV_ARCH-$ENV_OS"
+XMONAD_EXE_PATH="$XMONAD_CACHE_DIR/$XMONAD_NAME"
 
 cabal install "exe:$PROF_ID" \
   --installdir="$XMONAD_CACHE_DIR" --install-method=copy \
